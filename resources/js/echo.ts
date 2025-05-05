1cdb089487e2117e1d8e70655d822411ca6887d3
Min Khant Naung <minkhantnaungkzw@gmail.com>
2025-05-05 22:18:31 +0630

    Fix: Echo error for typescript by changing js to ts and updating

--- resources/js/echo.ts
+++ resources/js/echo.ts
@@ -1,7 +1,16 @@
 import Echo from 'laravel-echo';
 
 import Pusher from 'pusher-js';
+
+
 window.Pusher = Pusher;
+
+declare global {
+    interface Window {
+        Pusher: any;
+        Echo: Echo<any>;
+    }
+}
 
 window.Echo = new Echo({
     broadcaster: 'reverb',

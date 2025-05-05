7a348567f559c18800399f66b0e0b73a321820c8
Min Khant Naung <minkhantnaungkzw@gmail.com>
2025-05-05 22:18:46 +0630

    Add: MessageSent Event

---
+++ app/Events/MessageSent.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\ChatMessage;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class MessageSent implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    protected ChatMessage $chatMessage;
+
+    public function __construct(ChatMessage $chatMessage) {
+        $this->chatMessage = $chatMessage;
+    }
+
+    public function broadcastWith(): array
+    {
+        return [
+            'chatMessage' => $this->chatMessage
+        ];
+    }
+
+    public function broadcastOn(): array
+    {
+        return [
+            new PrivateChannel('chat.' . $this->chatMessage->receiver_id),
+        ];
+    }
+}

fceca22363b5ae2e89666ed6810a232bc939311a
Min Khant Naung <minkhantnaungkzw@gmail.com>
2025-05-13 22:17:25 +0630

    Update: import Deferred component to Dashboard.vue, redesign

--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import AppLayout from '@/layouts/AppLayout.vue';
 import { type BreadcrumbItem } from '@/types';
-import { Head, Link } from '@inertiajs/vue3';
+import { Deferred, Head, Link } from '@inertiajs/vue3';
 
 defineProps<{ users: { id: number; name: string; email: string }[] }>();
 
@@ -17,23 +17,21 @@ const breadcrumbs: BreadcrumbItem[] = [
   <Head title="Dashboard" />
 
   <AppLayout :breadcrumbs="breadcrumbs">
-    <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
+    <div class="grid auto-rows-min gap-4 md:grid-cols-3 h-full rounded-xl p-4">
       <Deferred data="users">
         <template #fallback>
           <div>Loading...</div>
         </template>
 
-        <div class="grid auto-rows-min gap-4 md:grid-cols-3">
-          <Link
-            :href="route('chat', user)"
-            v-for="user in users"
-            :key="user.id"
-            class="border-sidebar-border/70 dark:border-sidebar-border relative flex aspect-video cursor-pointer flex-col items-center justify-center overflow-hidden rounded-xl border"
-          >
-            <h3>{{ user.name }}</h3>
-            <p>{{ user.email }}</p>
-          </Link>
-        </div>
+        <Link
+          :href="route('chat', user)"
+          v-for="user in users"
+          :key="user.id"
+          class="border-sidebar-border/70 dark:border-sidebar-border relative flex aspect-video cursor-pointer flex-col items-center justify-center overflow-hidden rounded-xl border"
+        >
+          <h3>{{ user.name }}</h3>
+          <p>{{ user.email }}</p>
+        </Link>
       </Deferred>
     </div>
   </AppLayout>

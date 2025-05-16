eb024874e6adc05b0bc9c97f2e9b09a69c364c8e
Min Khant Naung <minkhantnaungkzw@gmail.com>
2025-05-16 23:31:08 +0630

    Update: show development info in Dashboard page

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -10,12 +10,6 @@ final class DashboardController extends Controller
 {
     public function __invoke()
     {
-        $users = User::query()
-            ->whereNot('id', Auth::id())
-            ->paginate(6);
-
-        return Inertia::render('Dashboard', [
-            'users' => $users
-        ]);
+        return Inertia::render('Dashboard');
     }
 }
--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -1,18 +1,7 @@
 <script setup lang="ts">
 import AppLayout from '@/layouts/AppLayout.vue';
 import { type BreadcrumbItem } from '@/types';
-import { Head, Link, usePage } from '@inertiajs/vue3';
-import Pagination from '@/components/Pagination.vue';
-import { onMounted, onUnmounted } from 'vue';
-import { useToast } from "vue-toastification";
-import type { SharedData } from '@/types';
-
-const toast = useToast();
-const page = usePage<SharedData>()
-
-defineProps({
-    users: Object
-});
+import { Head } from '@inertiajs/vue3';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -20,36 +9,14 @@ const breadcrumbs: BreadcrumbItem[] = [
         href: '/dashboard',
     },
 ];
-
-onMounted(() => {
-    window.Echo.private(`chat.${page.props.auth.user.id}`)
-    .listen("MessageSent", (response: any) => {
-        const incomingMessage = response.chatMessage;
-        toast.info(`${incomingMessage.sender.name}: ${incomingMessage.message}`);
-    })
-})
-
-onUnmounted(() => {
-    window.Echo.private(`chat.${page.props.auth.user.id}`).stopListening("MessageSent");
-})
 </script>
 
 <template>
-  <Head title="Dashboard" />
+    <Head title="Dashboard" />
 
-  <AppLayout :breadcrumbs="breadcrumbs">
-    <div class="grid auto-rows-min gap-4 md:grid-cols-3 h-full rounded-xl p-4">
-      <Link
-        :href="route('chat', user)"
-        v-for="user in users?.data"
-        :key="user.id"
-        class="border-sidebar-border/70 dark:border-sidebar-border relative flex aspect-video cursor-pointer flex-col items-center justify-center overflow-hidden rounded-xl border"
-      >
-        <h3>{{ user.name }}</h3>
-        <p>{{ user.email }}</p>
-      </Link>
-    </div>
-
-    <Pagination class="mt-3" :links="users?.links" />
-  </AppLayout>
+    <AppLayout :breadcrumbs="breadcrumbs">
+        <div class="grid h-full auto-rows-min gap-4 rounded-xl p-4 md:grid-cols-3">
+            This dashboard page is under development.
+        </div>
+    </AppLayout>
 </template>

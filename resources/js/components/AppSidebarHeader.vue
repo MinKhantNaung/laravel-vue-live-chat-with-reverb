c301b9e33ee424da1e9afb1172921de02463b797
Min Khant Naung <minkhantnaungkzw@gmail.com>
2025-05-16 23:07:35 +0630

    Refactor: just format code in AppSideBarHeader.vue

--- resources/js/components/AppSidebarHeader.vue
+++ resources/js/components/AppSidebarHeader.vue
@@ -1,29 +1,32 @@
 <script setup lang="ts">
-import Breadcrumbs from '@/components/Breadcrumbs.vue';
-import { SidebarTrigger } from '@/components/ui/sidebar';
-import type { BreadcrumbItemType, User } from '@/types';
+import Breadcrumbs from "@/components/Breadcrumbs.vue";
+import { SidebarTrigger } from "@/components/ui/sidebar";
+import type { BreadcrumbItemType, User } from "@/types";
 
-withDefaults(defineProps<{
+withDefaults(
+  defineProps<{
     breadcrumbs?: BreadcrumbItemType[];
     friend?: User | null;
-}>(),{
+  }>(),
+  {
     breadcrumbs: () => [],
-    friend: null
-});
+    friend: null,
+  }
+);
 </script>
 
 <template>
-    <header
-        class="flex h-16 shrink-0 justify-between items-center gap-2 border-b border-sidebar-border/70 px-6 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 md:px-4"
-    >
-        <div class="flex items-center gap-2">
-            <SidebarTrigger class="-ml-1" />
-            <template v-if="breadcrumbs && breadcrumbs.length > 0">
-                <Breadcrumbs :breadcrumbs="breadcrumbs" />
-            </template>
-        </div>
-        <div v-if="friend">
-            {{ friend.name }}
-        </div>
-    </header>
+  <header
+    class="flex h-16 shrink-0 justify-between items-center gap-2 border-b border-sidebar-border/70 px-6 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 md:px-4"
+  >
+    <div class="flex items-center gap-2">
+      <SidebarTrigger class="-ml-1" />
+      <template v-if="breadcrumbs && breadcrumbs.length > 0">
+        <Breadcrumbs :breadcrumbs="breadcrumbs" />
+      </template>
+    </div>
+    <div v-if="friend">
+      {{ friend.name }}
+    </div>
+  </header>
 </template>
